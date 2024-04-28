72f6cdbaf855178f001223eb76a6757e8e0822ce
Mohammed Sameed Mohammed Ishaque <91049385+MohammedSameed@users.noreply.github.com>
2024-04-28 10:38:28 +0530

    AddServerImpl.java

--- RMI/AddServerImpl.java
+++ RMI/AddServerImpl.java
@@ -1,13 +1,4 @@
 
-//============================================================================
-//
-//  Name        : AddServerImpl.java
-//  Author      : Sachin Vijaykumar Kunte
-//  Copyright   : Copyright ©2019 by Sachin Kunte All rights reserved.
-//  Description : A Java program for a server side function implementation 
-//  contact     : kuntesv@gmail.com
-//
-//============================================================================
 
 import java.rmi.*;
 import java.rmi.server.*;
